06e73653ae57ac594ea32ec690dcc6e2cf01a07a
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2026-05-16 12:28:19 +0530

    Added Search endpoints with filter crieteria parameters

--- src/main/java/com/semicore/ecom/prd/svc/ScEcomProductsServiceApplication.java
+++ src/main/java/com/semicore/ecom/prd/svc/ScEcomProductsServiceApplication.java
@@ -1,6 +1,5 @@
 package com.semicore.ecom.prd.svc;
 
-import com.semicore.ecom.prd.svc.controllers.ProductController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
@@ -13,10 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScEcomProductsServiceApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(ScEcomProductsServiceApplication.class, args);
 		/*ProductController pc = new ProductController();
-		System.out.println("Products: " + pc.getProducts())*/;
+		System.out.println("Products: " + pc.getProducts());*/
 	}
 
 }
